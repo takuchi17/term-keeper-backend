6d79c50fa121ef7b7950f509975df73f90f24db6
chihaya <takuchi.1071880@gmail.com>
2025-04-27 18:38:48 +0900

    update: データベースに関する環境変数などの設定を追加

--- configs/config.go
+++ configs/config.go
@@ -3,28 +3,44 @@ package configs
 import (
 	"log/slog"
 	"os"
+	"strconv"
 )
 
 type ConfigList struct {
 	Env                  string
+	DBUser               string
+	DBHost               string
+	DBPort               int
+	DBName               string
+	DBPassword           string
 	APICorsAllowsOrigins []string
 }
 
-var Configs ConfigList
+var Config ConfigList
 
 func (c *ConfigList) IsDevelopment() bool {
 	return c.Env == "development"
 }
 
 func LoadEnv() error {
-	Configs = ConfigList{
-		Env:                  GetEnvDefault("APP_ENV", "development"),
+	DBPort, err := strconv.Atoi(getEnvDefault("MYSQL_PORT", "3306"))
+	if err != nil {
+		return nil
+	}
+
+	Config = ConfigList{
+		Env:                  getEnvDefault("APP_ENV", "development"),
+		DBUser:               getEnvDefault("DB_USER", "user"),
+		DBHost:               getEnvDefault("DB_HOST", "localhost"),
+		DBPort:               DBPort,
+		DBName:               getEnvDefault("DB_NAME", "term_keeper_db"),
+		DBPassword:           getEnvDefault("DB_PASSWORD", "password"),
 		APICorsAllowsOrigins: []string{"http://localhost:3001"},
 	}
 	return nil
 }
 
-func GetEnvDefault(key, defVal string) string {
+func getEnvDefault(key, defVal string) string {
 	val, ok := os.LookupEnv(key)
 	if !ok {
 		return defVal
--- configs/config_test.go
+++ configs/config_test.go
@@ -10,6 +10,11 @@ func TestInitEnv(t *testing.T) {
 	err := LoadEnv()
 	assert.Nil(t, err)
 
-	assert.Equal(t, "development", Configs.Env)
-	assert.Equal(t, []string{"http://localhost:3001"}, Configs.APICorsAllowsOrigins)
+	assert.Equal(t, "development", Config.Env)
+	assert.Equal(t, "user", Config.DBUser)
+	assert.Equal(t, "localhost", Config.DBHost)
+	assert.Equal(t, "term_keeper_db", Config.DBName)
+	assert.Equal(t, "password", Config.DBPassword)
+	assert.Equal(t, 3306, Config.DBPort)
+	assert.Equal(t, []string{"http://localhost:3001"}, Config.APICorsAllowsOrigins)
 }
